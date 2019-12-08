0833adef78de8202ea730a2a4c514d1beeab93c6
monomania <szy.foggy@gamil.com>
2019-12-08 19:55:38 +0800

    1.加入测试代码

--- FOOT000TestApplication.go
+++ FOOT000TestApplication.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"math/rand"
+	"tesou.io/platform/foot-parent/foot-web/module/leisu/constants"
 	"tesou.io/platform/foot-parent/foot-web/module/leisu/service"
+	"tesou.io/platform/foot-parent/foot-web/module/leisu/utils"
 )
 
 func main() {
@@ -14,6 +18,19 @@ func main() {
 		fmt.Println(intn)
 	}
 	//测试从雷速获取可发布的比赛池
+	readCloser := utils.Get(constants.MATCH_LIST_URL)
+	reader := bufio.NewReader(readCloser)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break;
+		} else if err != nil {
+			fmt.Println(err)
+		}else{
+			fmt.Println(string(line))
+		}
+	}
+	//
 	poolService := new(service.MatchPoolService)
 	list := poolService.GetMatchList()
 	for _, e := range list {
