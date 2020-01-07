8baae1df3e7c942429e65106574a0a8aa9649968
shi.zeyuan <shizeyuan@bozhoutech.com>
2020-01-07 21:35:16 +0800

    1.xxxx

---
+++ foot-api/module/match/pojo/BaseFaceFutureEvent.go
@@ -0,0 +1,38 @@
+package pojo
+
+import (
+	"tesou.io/platform/foot-parent/foot-api/common/base/pojo"
+	"time"
+)
+
+/**
+未来赛事
+*/
+type BaseFaceFutureEvent struct {
+	//比赛id
+	MatchId string `xorm:"comment('比赛ID')"`
+
+	/**
+	 * 比赛时间
+	 */
+	EventMatchDate time.Time `xorm:"unique(EventMatchDate_EventMainTeamId_EventGuestTeamId)"`
+	/**
+	 * 联赛Id
+	 */
+	EventLeagueId string `xorm:" comment('联赛Id') index"`
+	/**
+	 * 主队id,目前为主队名称
+	 */
+	EventMainTeamId string `xorm:"unique(EventMatchDate_EventMainTeamId_EventGuestTeamId)"`
+	/**
+	 * 客队id,目前为客队名称
+	 */
+	EventGuestTeamId string `xorm:"unique(EventMatchDate_EventMainTeamId_EventGuestTeamId)"`
+	/**
+	间隔天数
+	*/
+	IntervalDay int `xorm:" comment('间隔天数') index"`
+
+
+	pojo.BasePojo `xorm:"extends"`
+}
---
+++ foot-api/module/match/pojo/BaseFaceScore.go
@@ -0,0 +1,41 @@
+package pojo
+
+import "tesou.io/platform/foot-parent/foot-api/common/base/pojo"
+
+/**
+联赛积分
+*/
+type BaseFaceScore struct {
+	//比赛id
+	MatchId string `xorm:"comment('比赛ID')"`
+	/**
+	 * 主队id,目前为主队名称
+	 */
+	TeamId string `xorm:"comment('球队ID') index"`
+
+	// 0 总 , 1 主 ,2客 3 近
+	Type int `xorm:"comment('类型:0 总 , 1 主 ,2客 3 近') index"`
+
+	//比赛场次
+	MatchCount int `xorm:"comment('比赛场次') index"`
+
+	//胜次数
+	WinCount  int `xorm:"comment('胜次数') index"`
+	DrawCount int `xorm:"comment('平次数') index"`
+	FailCount int `xorm:"comment('败次数') index"`
+
+	//进球
+	GetGoal  int `xorm:"comment('进球') index"`
+	LossGoal int `xorm:"comment('丢球') index"`
+	DiffGoal int `xorm:"comment('净胜球') index"`
+
+	//得分
+	Score int `xorm:"comment('进球') index"`
+
+	//排名
+	Ranking int `xorm:"comment('排名') index"`
+
+
+
+	pojo.BasePojo `xorm:"extends"`
+}
