0d6c6f6ccfe828c6d7952a7af583093bb9f06f0f
monomania <szy.foggy@gamil.com>
2020-03-04 09:09:21 +0800

    1.增加统计sql

--- link_stat_win.sql
+++ link_stat_win.sql
@@ -2,17 +2,19 @@ SELECT
   ar.`AlFlag` AS '模型',
   DATE_FORMAT(ar.`MatchDate`, '%w') AS '星期',
   ar.`LetBall` AS '盘口',
-  COUNT(1) AS '场次',
-  ar.`Result` AS '结果' 
+  ar.`Result` AS '结果' ,
+  COUNT(1) AS '场次'
+
 FROM
   foot.`t_analy_result` ar,
-  foot.`t_match_his` mh 
-WHERE ar.`MatchId` = mh.`Id` 
-  #and mh.`MainTeamGoals` = mh.`GuestTeamGoals` 
-  AND ar.`TOVoidDesc` = '' 
+  foot.`t_match_his` mh
+WHERE ar.`MatchId` = mh.`Id`
+  #and mh.`MainTeamGoals` = mh.`GuestTeamGoals`
+  AND ar.`TOVoidDesc` = ''
 GROUP BY ar.`AlFlag`,
   ar.`Result`,
-  DATE_FORMAT(ar.`MatchDate`, '%w') 
+  DATE_FORMAT(ar.`MatchDate`, '%w')
+
 ORDER BY ar.`AlFlag`,
   DATE_FORMAT(ar.`MatchDate`, '%w'),
   ar.`LetBall`,
