f7e7227eb0b2681e5c0cd31892236c5a4203ef2b
shi.zeyuan <shizeyuan@bozhoutech.com>
2020-04-10 20:56:55 +0800

    1.blog

--- build_linux.bat
+++ build_linux.bat
@@ -1,17 +1,21 @@
 @echo off
 echo=
 
+echo set sytem env
 set GOOS=linux
 set GOARCH=amd64
 
+echo copy config
 if not exist %cd%\bin\conf\ md %cd%\bin\conf\
 xcopy /Y /S %cd%\conf %cd%\bin\conf\
 xcopy /Y /S %cd%\conf\app_prod.ini %cd%\bin\conf\app.ini
 xcopy /Y /S %cd%\conf\app_pord.conf %cd%\bin\conf\app.conf
 
+echo copy resource
 if not exist %cd%\bin\assets\ md %cd%\bin\assets\
 xcopy /Y /S %cd%\assets %cd%\bin\assets\
 
+echo build
 go build -v -x -o bin/FOOT000 FOOT000.go
 
 echo=
--- build_windows.bat
+++ build_windows.bat
@@ -1,16 +1,18 @@
 @echo off
 echo=
 
+echo copy config
 if not exist %cd%\bin\conf\ md %cd%\bin\conf\
 xcopy /Y /S %cd%\conf %cd%\bin\conf\
 xcopy /Y /S %cd%\conf\app_prod.ini %cd%\bin\conf\app.ini
 xcopy /Y /S %cd%\conf\app_pord.conf %cd%\bin\conf\app.conf
 
+echo copy resource
 if not exist %cd%\bin\assets\ md %cd%\bin\assets\
 xcopy /Y /S %cd%\assets %cd%\bin\assets\
 
+echo build
 go build  -v -x  -o bin/FOOT000.exe FOOT000Cmd.go
-
 
 echo=
 pause
